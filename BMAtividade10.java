d090ac003ef0031cafb5a515d003d8bf2937024f
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2023-04-12 22:23:10 -0300

    Create BMAtividade10.java

---
+++ BMAtividade10.java
@@ -0,0 +1,11 @@
+public class BMAtividade10
+{
+    public static void main(String[] args){
+        int x=10;
+        System.out.println("" +x);
+        do {
+            x++;
+            System.out.println("" +x);
+        } while (x<30);
+    }
+}
